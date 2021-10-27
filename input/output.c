102735b690575be36b499088013277a3ad8ea770
yogesh joga <yjoga27@gmail.com>
2021-10-27 17:52:02 +0530

    input/output

---
+++ input/output.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+int main(){
+    char a,b;
+    b = (long) 'i love you rose';
+    printf("Enter your girlfriend name :");
+    scanf("%c", &a);
+    printf("%c , %c", a, b);
+
+
+}
+
+
