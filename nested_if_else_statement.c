dacdb45d42da02d8515e6a415667967ba7ba663f
yogesh joga <yjoga27@gmail.com>
2021-10-28 10:59:15 +0530

    very fun with c lang code awh awh

---
+++ nested_if_else_statement.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+//now start the nested if-else statement don't waste the time let's start the code!!!
+void main(){
+    int a,b;
+    printf("Enter the 1st value :");
+    scanf("%d", &a);
+    printf("Enter the 2nd value :");
+    scanf("%d", &b);
+    // start the nested if statement block
+    if (a ==20) // 1st if block stared 
+    {
+        if (b ==30){   // 2nd inner if block 
+            printf("A is the 20, and B is teh 30 it's true value's hy hy hy.."); // print block 
+        }
+    }
+        else{
+            printf("A and B value's are not matched"); // else block
+        }
+}
+
+// today having very fun with C lang code awh awh awh awh......................
+
