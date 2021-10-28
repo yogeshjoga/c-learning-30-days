3c64fa1c2e3a1c35ec241e76575e58ed09311fea
yogesh joga <yjoga27@gmail.com>
2021-10-28 22:20:29 +0530

    for loop completed

---
+++ For_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+// for loop  
+void main(){  //main method
+    int a,b; // integer
+    b=100;  // storing
+    for(a=1; a<=b; a++ ){  // loop starting 
+        printf("For loop value's : %d \n", a); //loop calling
+    }
+
+}
+// i want more question to solve in for loop experssion 
+// any way thank you for watching my code's
