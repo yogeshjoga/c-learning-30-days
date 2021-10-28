ba3bfd3ffee34fc904521d3b55b463bfbfcf3bc4
yogesh joga <yjoga27@gmail.com>
2021-10-28 15:23:04 +0530

    do while loop completed

---
+++ do_while.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+//do while loop start in this code session
+void main(){
+    int a = 0, b = 100; 
+    do{  //do loop mean task given the do! just it can do only !!
+        printf("Do while loop %d \n", a); //import the values from int   i can imported  int a 
+        a = a + 1;  // given task to do loop it will do untill conditaion satfactions
+    }
+    while(a<=b); //satfication condition on the while loop upto 'b' to satfactions
+
+}
+ // any way thankyou all
+ // im learn from udemy and w3schools c lang thankyou these two
