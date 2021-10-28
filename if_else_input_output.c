964c034fc9210b053143788bcda752bd1bdf3dda
yogesh joga <yjoga27@gmail.com>
2021-10-28 10:06:47 +0530

    happy day learning good code logics

---
+++ if_else_input_output.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+// if-else statement by usign inputs from taken in user
+int main(){
+    int a,b;  //values 
+    printf("Enter a number :");    //print the value.
+    scanf("%d", &a);          // taken a 1st value from the user.
+    printf("Enter 2nd number :"); // asking to user 2nd value.
+    scanf("%d", &b);    //taken a 2nd value from the user.
+
+    if(a > b){  //if condiation starting 
+        printf("A grather B %d", a); // output printing the what you expert the value
+    }
+    else{  // else conditon starting 
+        printf("B is grather A %d", b);
+    }
+}
+
+ // thankyou im yogesh joga im learining C programming..
+ // thankyou for wathing my code's
