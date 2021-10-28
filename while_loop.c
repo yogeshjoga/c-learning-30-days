3347977732b188227924d5572e2d1f585acc21d7
yogesh joga <yjoga27@gmail.com>
2021-10-28 14:44:02 +0530

    loops while loop completed, learnt from w3schools and udemy thankyou these free sources

---
+++ while_loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+//loops 1st while loop 
+//loop task is 1 to 100 number's print by using C programming lang
+void main(){
+    int a=1, b=100; //integers 
+    while(a <= b){  // while loop started now and take a condiation
+        printf("This is the While loop %d \n", a); // print the lower value of integer 
+        a++; //A int to increment upto B value is 100, so upto 100 values
+    }
+    
+}
+// notes
+// * while loop is a most basic loop in C programming. while loop has one control condition,
+// It executes as long the condition is true.  The condition of the loop is tested before the body of the loop is executed 
+// C while loops statement allows to repeatedly run the same block of code until a condition is met.
+// thank you today learnt a lot bye bye see you later.
