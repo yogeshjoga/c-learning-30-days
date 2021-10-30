9d33a3eb1426218a9a73571c8a165947ec13d593
yogesh joga <yjoga27@gmail.com>
2021-10-30 15:26:32 +0530

    char array

---
+++ char_array_input_output.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+// this is a small code for your girl friend to propse.
+void main(){
+    char b[20]; // 1st char array is "b" and size of the char length is [20] 20 charaters
+    char a[20]="I LOVE YOU ROSE"; // 2nd char array is "a" 
+    printf("Enter your girlfriend name :");
+    scanf("%s", &b);
+    printf(" %s miss %s darling", a, b);
+   
+}
+
+// its something was wrong i don't know now, when i will learnt i will update.
+// lets rock
+
+
+
