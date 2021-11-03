aaf08dd32957d79a2ecf88befa63096e0870e987
yogesh joga <yjoga27@gmail.com>
2021-11-03 09:35:00 +0530

    project

--- projects/Clock.c
+++ projects/Clock.c
@@ -11,26 +11,21 @@ int main(){   //main
     }
     while(1){  //  while loop
         s++;
-        if (s>59){    // if statment
-            m++;
-            s=0;
+        if (s>59){    // if statment for seconds not exced to 60 count
+            m++;   // minutes increment
+            s=0;   // again seconds will zero
         }
-        if(m>59){
-            h++;
-            m=0;
+        if(m>59){      // min not exced to 60 count
+            h++;       // hours increment
+            m=0;         // min will restart to zero
         }
-        if(h>24){
-            h=1;
+        if(h>24){   // hours not exced to 24 hr's
+            h=1;   // hours will restart
         }
-        printf("\n Time right now :");
-        printf("%02d:%02d:%02d", s,m,h);
-        Sleep(d);
-        system("cls");
+        printf("\n Time right now :");   // print will be excuted 
+        printf("%02d:%02d:%02d", s,m,h);   // Clock will display
+        Sleep(d);        // sleep is delay to the counting
+        system("cls");   // clear screen to updated values only displayed.
     }
 }
-
-
-
-
-
-
+ // have a nice day dear
