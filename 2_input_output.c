2343258d5d043dcf4ed7b6cf1d062c0a7715726c
yogesh joga <yjoga27@gmail.com>
2021-11-05 13:06:42 +0530

    new input/output code for name and age

---
+++ 2_input_output.c
@@ -0,0 +1,11 @@
+//inputs/outputs
+#include<stdio.h>
+
+int main(){
+char stg[100];
+int I;
+printf("Enter your name and your age\n");
+scanf("%s %d", &stg, &I);
+printf("your name is %s \nyour age is :%d", stg, I);
+return 0;
+}
