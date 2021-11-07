4298844546e4831bb3798edf4468f46906153feb
yogesh joga <yjoga27@gmail.com>
2021-11-07 09:47:31 +0530

    math

--- math.c
+++ math.c
@@ -4,9 +4,9 @@
 // task is find the average of all the numbers in between 16 and 304 which is divisible by 5
 int main(){
     int i;
-    for(i=16;i<=304; i++){
-        if(i % 5 == 0){
-            printf("\n Divisible by 5 : %d",i);
+    for(i=16;i<=304; i++){ // counting the number 16 to 304
+        if(i % 5 == 0){  // cheacking the divisible into 5=0;
+            printf("\n Divisible by 5 : %d",i); // print which is divisible into 5; print only
         }
     }
 }
